4a324044fa78fa0ef2df5caffa37e358399f0a58
DeusVoult <118910056+DeusVoult@users.noreply.github.com>
2023-01-23 16:48:05 +0100

    Add files via upload

---
+++ ETAPE1/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { TachesComponent } from './component/taches/taches.component';
+import { FiltreTachePipe } from './pipe/filtre-tache.pipe';
+import {FiltreTacheundefinedPipe} from './pipe/filtre-tache.pipe';
+import {FiltreTachependingPipe} from './pipe/filtre-tache.pipe';
+import {FiltreTacheIPPipe} from './pipe/filtre-tache.pipe';
+import {FiltreTacheCPPipe} from './pipe/filtre-tache.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    TachesComponent,
+    FiltreTachePipe,
+    FiltreTacheundefinedPipe,
+    FiltreTachependingPipe,
+    FiltreTacheIPPipe,
+    FiltreTacheCPPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+ ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
